3bbe6bb829c58cc4ed2413d8a6e0ff4a13da9e09
백용호 <yongyong@sqisoft.com>
2020-10-16 10:24:15 +0900

    python 교육하던 내용 추가

---
+++ learnPy/python_intro/main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def hello():
+    print("hello")
+    print("welcome")
+    print("""1.     
+2.
+3.
+""")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+hello()
+
+c = "5"
+d = "8"
+
+print(c + d)
+
+print(7 / 2)
+
+print(7 // 2)
